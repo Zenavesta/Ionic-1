b074e086bbc570ea41e226bf6d94f2f94b18156d
Alberth Pérez Mamani <zenavesta123@gmail.com>
2024-05-12 15:16:45 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
 
+
+export const routes = [
+  { path: 'main', loadComponent: () => import('./main/main.page').then(m => m.MainPage) },
+  {
+    path: 'product-list',
+    loadComponent: () => import('./product-list/product-list.page').then( m => m.ProductListPage)
+  }
 ];
