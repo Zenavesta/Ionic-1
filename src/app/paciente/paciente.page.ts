4c6775ecfd48509e4605bf1f7f1a7f55c8338c30
Alberth Pérez Mamani <zenavesta123@gmail.com>
2024-05-17 21:44:29 -0500

    Cambio de estilo en paciente

--- src/app/paciente/paciente.page.ts
+++ src/app/paciente/paciente.page.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonItem, IonLabel, IonRadio, IonCol, IonButton, IonInput, IonRadioGroup } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonItem, IonLabel, IonRadio, IonCol, IonButton, IonInput, IonRadioGroup, IonSelect, IonSelectOption, IonGrid } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-paciente',
   templateUrl: './paciente.page.html',
   styleUrls: ['./paciente.page.scss'],
   standalone: true,
-  imports: [IonRadioGroup, IonInput, IonButton, IonCol, IonRadio, IonLabel, IonItem, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonGrid,
+    IonRadioGroup, IonInput, IonButton, IonCol, IonRadio, IonLabel, IonItem, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSelect, IonSelectOption
+  ]
 })
 export class PacientePage implements OnInit {
 
